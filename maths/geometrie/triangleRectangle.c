e6770eceb3fd612498e63ed0fca6cf85b6bcb410
CharafO <ouciblec@gmail.com>
2025-01-06 00:01:56 +0100

    2 triangleRectangle v0.1

--- maths/geometrie/triangleRectangle.c
+++ maths/geometrie/triangleRectangle.c
@@ -1,10 +1,12 @@
-int main(){
-    char* phrase = triangleRectangle(3, 4, 5);
-    printf("%s", phrase);
-    return 0;
-}
+#include <stdio.h>
 
 char* triangleRectangle(int a, int b, int c){
-    if(a*a + b*b == c*c) return "C'est un triangle rectangle.";
-    else return "Ce n'est PAS un triangle rectangle.";
+    if(a*a + b*b == c*c) return "C'est un triangle rectangle.\n";
+    else return "Ce n'est PAS un triangle rectangle.\n";
+}
+
+int main(){
+    char* phrase = triangleRectangle(3, 4, 6);
+    printf("%s", phrase);
+    return 0;
 }
